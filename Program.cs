0006783fc7cea52a7771249e07e154eccc0361d0
sulemanabdi <suleman.abdi2001@hotmail.com>
2025-05-15 00:21:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using  AbdiHotelConsole.Data;
+using AbdiHotelLibrary;
+
+
+namespace  AbdiHotelConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            app.Run();
+
+            using (var dbContext = new ApplicationDbContext())
+            {
+                var rec = new Reception();
+                rec.ReceptionMenu();
+            }
+        }
+    }
+}
